e91dca980062678b93d1f34d9fd47807d91bd2b3
meenakshilakshmanan12 <42211002+meenakshilakshmanan12@users.noreply.github.com>
2018-08-27 20:48:01 +0530

    Add files via upload

---
+++ FirstTask.java
@@ -0,0 +1,40 @@
+public class FirstTask{ 
+
+
+
+    public static String concatWithStr()    {  
+        String t = "Meenakshi";  
+        for (int i=0; i<100; i++){  
+            t = t + "Lakshmanan";  
+        }  
+        return t;  
+    }  
+    public static String concatWithStrBuf(){  
+        StringBuffer sb = new StringBuffer("Meenakshi");  
+        for (int i=0; i<100; i++){  
+            sb.append("Lakshmanan");  
+        }  
+        return sb.toString();  
+    }   
+    public static void main(String args[]){ 
+        //test for time taken
+	    long startTime = System.currentTimeMillis();  
+        concatWithStr();  
+        System.out.println("Time taken by Concating with String: "+(System.currentTimeMillis()-startTime)+"ms"); //takes some time 
+        startTime = System.currentTimeMillis();  
+        concatWithStrBuf();  
+        System.out.println("Time taken by Concating with  StringBuffer: "+(System.currentTimeMillis()-startTime)+"ms"); //concats within no time
+		
+        //hashcode test  
+        String str="Meenakshi";  
+        System.out.println(str.hashCode());  // generates the hashCode of str
+        str=str+"Lakshmanan";  
+        System.out.println(str.hashCode());  //provides new hashCode for str
+   
+        
+        StringBuffer sb=new StringBuffer("Meenakshi");  
+        System.out.println(sb.hashCode());  //generates the hashCode of str
+        sb.append("Lakshmanan");  
+        System.out.println(sb.hashCode());  //gives the same hashCode as before
+    }  
+}  
