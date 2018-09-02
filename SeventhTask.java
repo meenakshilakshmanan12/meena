48443c88e05160a20340d1c63ac1380184aed6f3
meenakshilakshmanan12 <42211002+meenakshilakshmanan12@users.noreply.github.com>
2018-09-02 14:00:52 +0530

    Seventh Task
    to remove the numreic values and the mentioned substring from the main string

--- SeventhTask.java
+++ SeventhTask.java
@@ -5,9 +5,9 @@ public class SeventhTask{
 		Scanner scan = new Scanner(System. in);
 	    System.out.println("enter the string:");
 		String s = scan.nextLine();
-		System.out.println("Before removing the numbers:" + s);
+		System.out.println("Before removing the numbers and substring: " + s);
 		s =s.replaceAll("[0-9]","").replaceAll("java","");
-		System.out.println("After removing the numbers:" + s);
+		System.out.println("After removing the numbers and substring: " + s);
 	}
 }
 		
